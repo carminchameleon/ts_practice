83db10ffab34fc25299e2c2947e23f69075d7979
carminchameleon <hwanggee1211@gmail.com>
2020-07-04 20:12:26 +0900

    faker 설치, type 정의

--- src/User.ts
+++ src/User.ts
@@ -1,11 +1,19 @@
 import faker from 'faker';
 
+// type definition file 을 매뉴얼로 해야한다. -> @type/{library name}
+// string 을 Number로 바꿀 때 사용한 메소드 : parseFloat()
+
 class User {
   name: string;
   location: {
     lat: number;
     lng: number;
   };
-
-  constructor() {}
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
 }
